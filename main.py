c73644f7165b720abdc462f8d380476be27a2880
lcgreenn <coberta30@gmail.com>
2026-03-28 03:43:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("bot iniciando...")
+import requests
+import time
+
+TOKEN = "8781756079:AAF39To2Wh_v8IM1koM14nLQHDK-WTIyPJI"
+CHAT_ID = "@lcgreenbaccarat"
+
+def enviar(msg):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
+
+def sinal_teste():
+    msg = """
+🚨 BOT ONLINE
+
+✅ Sistema ativo
+📡 Enviando sinais automático
+
+🔥 Preparando análise...
+"""
+    enviar(msg)
+
+while True:
+    sinal_teste()
+    time.sleep(60)
